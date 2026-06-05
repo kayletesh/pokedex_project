cbf0d7ec73e0d54535c6999918c9c094677cb92c
kayletesh <kayletesh@gmail.com>
2026-06-05 15:50:38 -0400

    add display pokemon function add displayPokemonSVG to play event listener add overlay to pokemon svgs

--- whos-that-pokemon.js
+++ whos-that-pokemon.js
@@ -6,29 +6,41 @@ async function getPokemonData(id) {
       throw new Error(`Response status: ${res.status}`);
     }
     const result = await res.json();
-    console.log(result.name);
+    return result;
   } catch (error) {
     console.error(error.message);
   }
 }
 
+function displayPokemonSVG(currentPokemon) {
+  document.querySelector("#pokemon-svg img").src =
+    `${currentPokemon.sprites.other.dream_world.front_default}`;
+}
+
 function randomID(min, max) {
   const minID = Math.ceil(min);
   const maxID = Math.floor(max);
-
   return Math.floor(Math.random() * (maxID - minID) + minID);
 }
-
 const play = document.querySelector("#play-button");
+
+async function buildPokemonElement(min, max) {
+  const currentPokemon = await getPokemonData(randomID(min, max));
+  displayPokemonSVG(currentPokemon);
+  play.innerText = "PLAY AGAIN";
+}
+
 play.addEventListener("click", () => {
   const dropdown = document.querySelector("#gen-selector");
   const selectedGen = dropdown.value;
-  console.log(selectedGen);
-
-  if (dropdown.value === "gen-one") {
-    getPokemonData(randomID(1, 151));
-  } else if (dropdown.value === "gen-two") {
-    getPokemonData(randomID(152, 252));
+  if (selectedGen === "gen-one") {
+    const genStart = 1;
+    const genEnd = 151;
+    buildPokemonElement(genStart, genEnd);
+  } else if (selectedGen === "gen-two") {
+    const genStart = 152;
+    const genEnd = 252;
+    buildPokemonElement(genStart, genEnd);
   }
 });
 
@@ -37,4 +49,6 @@ play.addEventListener("click", () => {
 // GEN TWO
 // const genTwo = getPokemonData(randomID(152, 252));
 
-// if dropdown.value = gen-one, then return getPokemonData(randomID(1, 151))
+// create a function that creates an image
+// based on the id pulled from getPokemonData,
+// and displays it in div pokemon-svg
