ff538ecd6aa3b3f7b0376dfe6ed834e7599e8d44
kayletesh <kayletesh@gmail.com>
2026-06-22 20:33:12 -0400

    add script.js to all html files fix errors in css adjust dropdown functionality

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function handleHeader() {
   const headerDropdown = document.querySelector("#header-nav");
   const headerMenuBtn = document.querySelector("#header-menu-btn");
-  console.log("hello world");
   headerMenuBtn.addEventListener("click", () => {
-    headerDropdown.classList.toggle("header__nav--height");
-    console.log("ITS WORKING");
+    const isExpanded = headerMenuBtn.getAttribute("aria-expanded") === "true";
+    headerMenuBtn.setAttribute("aria-expanded", !isExpanded);
+    headerDropdown.inert = isExpanded;
+    headerDropdown.classList.toggle("header__nav--expanded");
   });
 }
 
