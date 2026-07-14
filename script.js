9633aaee1859fdb10ee4fe1e6751e7f50ff5fc92
kayletesh <kayletesh@gmail.com>
2026-07-14 17:28:50 -0400

    add basic javascript to find and call to each card

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 const isDesktop = window.matchMedia("(width >= 992px)");
-
+// #region header
 const headerDropdown = document.querySelector("#header-nav");
 const headerMenuBtn = document.querySelector("#header-menu-btn");
-
 let isOpen = false;
-
 headerMenuBtn.addEventListener("click", () => handleClick(isOpen));
-
 const handleClick = (state) => {
   isOpen = !state;
 
@@ -19,38 +16,23 @@ const handleClick = (state) => {
     headerDropdown.classList.remove("header__nav--expanded");
   }
 };
-
-// function handleHeader() {
 const closeNav = (state) => {
-  // headerMenuBtn.focus();
   isOpen = !state;
-
   headerMenuBtn.setAttribute("aria-expanded", !state);
   headerDropdown.inert = !state;
-  // headerDropdown.classList.toggle("header__nav--expanded");
+
   if (!state) {
     headerDropdown.classList.add("header__nav--expanded");
   } else {
     headerDropdown.classList.remove("header__nav--expanded");
   }
 };
-// }
-
-// handleHeader();
-
-// source of truth for styles
-// read screen size on start and resize to handle inert and aria-expanded
-
 const handleLoad = () => {
   if (isDesktop.matches) {
     headerDropdown.inert = false;
-
-    // console.log(true);
   } else {
-    // console.log(false);
   }
 };
-
 const handleChange = () => {
   if (isDesktop.matches) {
     closeNav(true);
@@ -58,6 +40,26 @@ const handleChange = () => {
     headerDropdown.inert = true;
   }
 };
-
 window.addEventListener("DOMContentLoaded", handleLoad);
 isDesktop.addEventListener("change", handleChange);
+// #endregion header
+
+// #region quiz-active
+const easyQuiz = document.querySelector("#quiz-card-green");
+const mediumQuiz = document.querySelector("#quiz-card-blue");
+const hardQuiz = document.querySelector("#quiz-card-yellow");
+const masterQuiz = document.querySelector("#quiz-card-pink");
+
+easyQuiz.addEventListener("click", () => {
+  console.log("green");
+});
+mediumQuiz.addEventListener("click", () => {
+  console.log("blue");
+});
+hardQuiz.addEventListener("click", () => {
+  console.log("yellow");
+});
+masterQuiz.addEventListener("click", () => {
+  console.log("pink");
+});
+// #endregion quiz-active
